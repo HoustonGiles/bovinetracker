bee806061fa178b5d17aeed659738ef6990bf067
Houston Giles <houston@xondesigns.com>
2015-11-22 04:07:55 -0600

    update routes

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,9 @@
 return [
     'default' => '/cow/list',
     'errors' => '/error',
+    'css' => '/css',
+    'fonts' => '/fonts',
+    'js' => '/js',
     'routes' => [
         '/cow(/:action(/:id))' => [
             'controller' => '\Bovinetracker\Controller\Cows',
